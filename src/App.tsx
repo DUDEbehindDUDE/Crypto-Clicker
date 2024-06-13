719ea8ca797a14e514f89f4937604ffbc4ccbec4
DUDEbehindDUDE <jnbrooks457@gmail.com>
2024-06-13 16:05:36 -0400

    Implement autosaves
    Autosaves every 15 seconds. If you want to reset your progress then just.. clear cookies?

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState, useEffect, createContext } from "react";
+import { useState, useEffect, createContext, useRef } from "react";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/shift-away.css";
 import "./App.css";
+import Cookies from "js-cookie";
 import React from "react";
-import MainCounters from "./counters/MainCounters.tsx"
+import MainCounters from "./counters/MainCounters.tsx";
 import Bitcoin from "./bitcoin/Bitcoin.tsx";
 import Store from "./store/Store.tsx";
 import SecondaryCounters from "./counters/SecondaryCounters.tsx";
@@ -19,10 +20,51 @@ function App() {
   const [bitcoinPerClick, setBitcoinPerClick] = useState<number>(1);
   const [ownedItems, setOwnedItems] = useState({
     // This is an object containing all the items the player owns and what upgrades they have purchased
-    hasChange: true,
+    hasChange: false,
     systems: {},
     upgrades: [],
   });
+
+  // UseRefs to keep track of autosave data
+  const hasLoadedRef = useRef(false);
+  const bitcoinsRef = useRef(bitcoins);
+  const ownedItemsRef = useRef(ownedItems);
+
+  // Load autosave data
+  useEffect(() => {
+    if (!hasLoadedRef.current) {
+      console.log("Loading Autosave Data");
+      hasLoadedRef.current = true;
+
+      const bitcoinsCookie = Cookies.get("bitcoins") as string | undefined;
+      const ownedItemsCookie = Cookies.get("ownedItems") as string | undefined;
+
+      if (bitcoinsCookie !== undefined && typeof bitcoinsCookie === "string") {
+        console.log(bitcoinsCookie);
+        setBitcoins(Number(bitcoinsCookie));
+      }
+
+      if (
+        ownedItemsCookie !== undefined &&
+        typeof ownedItemsCookie === "string"
+      ) {
+        console.log(ownedItemsCookie);
+        const parsed = JSON.parse(ownedItemsCookie);
+        parsed.hasChange = true;
+        console.log(parsed);
+        setOwnedItems({ ...parsed });
+        console.log("ownedItems:");
+        console.log(ownedItems);
+        // ownedItemsRecalculations();
+      }
+      console.log("Autosave Data Loaded!");
+    }
+  }, []);
+
+  // Update bitcoinsRef on bitcoins change
+  useEffect(() => {
+    bitcoinsRef.current = bitcoins;
+  }, [bitcoins]);
 
   // Add 1/10th of the bps to total bitcoin every 100ms
   useEffect(() => {
@@ -36,16 +78,48 @@ function App() {
     }
   }, [bps]);
 
+  // Autosave items to cookies every 15 secs
+  function doAutosave() {
+    // set data to expire 10 years from now
+    const expiryDate = new Date();
+    expiryDate.setFullYear(expiryDate.getFullYear() + 10);
+
+    // get items from ref, so they will be autosaved properly
+    Cookies.set("bitcoins", bitcoinsRef.current, { expires: expiryDate });
+    Cookies.set("ownedItems", JSON.stringify(ownedItemsRef.current), {
+      expires: expiryDate,
+    });
+
+    console.log("Autosave");
+    console.log(Cookies.get("bitcoins"));
+    console.log(JSON.parse(Cookies.get("ownedItems")));
+  }
+
+  useEffect(() => {
+    const autosaveInterval = setInterval(doAutosave, 1000 * 15); // 15 secs
+    return () => clearInterval(autosaveInterval);
+  }, []);
+
   // Recalculate BPS and bitcoin per click when owned items changes
   // note: set ownedItems.hasChange to true, otherwise it won't update
-  useEffect(() => {
+  function ownedItemsRecalculations() {
+    if (ownedItems.hasChange === false) return;
+
     const newBps = calculateBps(ownedItems.systems);
     const newBitcoinPerClick = calculateBtcPerClick(ownedItems.systems);
-    const resetChanges = ownedItems;
-    resetChanges.hasChange = false;
+    const resetChanges = {
+      ...ownedItems,
+      hasChange: false,
+    };
     setOwnedItems(resetChanges);
     setBps(newBps);
     setBitcoinPerClick(newBitcoinPerClick);
+
+    // update ownedItemsRef, to use in autosave data
+    ownedItemsRef.current = ownedItems;
+  }
+  useEffect(() => {
+    ownedItemsRecalculations();
   }, [ownedItems.hasChange, ownedItems]);
 
   // Calculates the bps every time owned items change
